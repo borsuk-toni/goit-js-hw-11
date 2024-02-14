833a66e807a432cb77a8c3dc7e8b05904eebec44
borsuk-toni <144048224+borsuk-toni@users.noreply.github.com>
2024-02-14 20:27:40 +0200

    revised

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,26 @@
+export function getImages(searchValue) {
+    const params = {
+        key: '42343629-db0a88f68e5938b107ae69266',
+        q: `${searchValue.trim()}`
+            .split(' ')
+            .map(value => {
+                return value.toLowerCase().trim();
+            })
+            .join('+'),
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+    };
+    const url = `https://pixabay.com/api/?key=${params.key}&q=${params.q}&image-type=${params.image_type}&orientation=${params.orientation}&safesearch=${params.safesearch}`;
+
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { createGallery } from "./js/render-functions.js";
+import { getImages } from "./js/pixabay-api.js";
+
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
@@ -11,69 +14,38 @@ loader.style.display = 'none';
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    const name = e.target.elements.input.value;
 
-    createGallery(name);
-    form.reset();
-});
+    gallery.innerHTML = '';
 
-function createGallery(searchValue) {
+    const input = e.target.elements.input.value.trim();
 
-    const params = {
-        key: '42343629-db0a88f68e5938b107ae69266',
-        q: `${searchValue.trim()}`
-            .split(' ')
-            .map(value => {
-                return value.toLowerCase().trim();
-            })
-            .join('+'),
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-    };
-    const url = `https://pixabay.com/api/?key=${params.key}&q=${params.q}&image-type=${params.image_type}&orientation=${params.orientation}&safesearch=${params.safesearch}`;
+    if (input === '') {
+        iziToast.warning({
+            message: 'Please enter a valid request!',
+            position: "topRight",
+        });
+        return;
+    }
 
     loader.style.display = 'block';
 
-    return fetch(url)
-        .then(res => res.json())
-        .then(result => {
-            if (result.hits.length === 0) {
-                loader.style.display = 'none';
-                gallery.innerHTML = '';
+    getImages(input)
+        .then(data => {
+            if (data.hits.length === 0) {
                 iziToast.error({
                     message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: "topRight",
                 });
             } else {
-                loader.style.display = 'none';
-                const markup = result.hits
-                    .map(image => {
-                        return `<li class="item"> <a class="gallery-item" href="${image.largeImageURL}">
-                            <img class="gallery-image"
-                            src="${image.webformatURL}"
-                            alt="${image.tags}"/>
-                        </a>
-                        <ul class="descr">
-                        <li class="descr-item"><p class="descr-title">Likes<span class="descr-value">${image.likes}</span></p></li>
-                        <li class="descr-item"><p class="descr-title">Views<span class="descr-value"> ${image.views}</span></p></li>
-                        <li class="descr-item"><p class="descr-title">Comments<span class="descr-value"> ${image.comments}</span></p></li>
-                        <li class="descr-item"><p class="descr-title">Downloads<span class="descr-value"> ${image.downloads}</span></p></li>
-                        </ul>
-                        </li>`;
-                    })
-                    .join('');
-                gallery.innerHTML = markup;
+                createGallery(data);
             }
-            const lightbox = new SimpleLightbox('.gallery a', {
-                captionsData: 'alt',
-                captionDelay: 250,
-            });
-            lightbox.refresh();
         })
         .catch(error => {
             console.log(error);
+        })
+        .finally(() => {
+            loader.style.display = 'none';
         });
-}
-
-
-
+    
+    e.target.reset();
+});
